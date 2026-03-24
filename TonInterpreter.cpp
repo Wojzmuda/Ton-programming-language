60234781802c52df7e72c3da40733f726e812383
kzdanski <zdanski.kamil@gmail>
2026-03-24 20:06:12 +0100

    TonInterpreter update

--- TonInterpreter.cpp
+++ TonInterpreter.cpp
@@ -1,4 +1,6 @@
 #include "TonInterpreter.h"
+#include <cmath>
+#include "AudioFile.h"
 
 std::any TonInterpreter::visitProgram(TonParser::ProgramContext *ctx){
     return visitChildren(ctx);
@@ -17,4 +19,127 @@ std::any TonInterpreter::visitVarDecl(TonParser::VarDeclContext *ctx){
     std::any value = visit(ctx->expr());
     memory[varName] = value;
     return value;
+}
+
+std::any TonInterpreter::visitIdExpr(TonParser::IdExprContext *ctx) {
+    std::string varName = ctx->ID()->getText(); 
+    if (memory.find(varName) != memory.end()) {
+        return memory[varName]; 
+    }
+
+    throw std::runtime_error("Error: Use of undefined variable '" + varName + "'.");
+}
+
+
+std::any TonInterpreter::visitAssignment(TonParser::AssignmentContext *ctx) {
+    std::string varName = ctx->ID()->getText();
+
+    if (memory.find(varName) == memory.end()) {
+        throw std::runtime_error("Error: Variable '" + varName + "' must be declared using !make before assignment.");
+    }
+
+    std::any newValue = visit(ctx->expr());
+    memory[varName] = newValue;
+
+    return {};
+}
+
+
+
+
+std::any TonInterpreter::visitShoutStat(TonParser::ShoutStatContext *ctx) {
+
+    std::any value = visit(ctx->expr());
+
+    if (value.type() == typeid(std::string)) {
+        std::cout << std::any_cast<std::string>(value) << std::endl;
+    }
+    else if (value.type() == typeid(int)) {
+        std::cout << std::any_cast<int>(value) << std::endl;
+    }
+    else if (value.type() == typeid(double)) {
+        std::cout << std::any_cast<double>(value) << std::endl;
+    }
+    else if (value.type() == typeid(char)) {
+        std::cout << "'" << std::any_cast<char>(value) << "'" << std::endl;
+    }
+    else if (value.type() == typeid(bool)) {
+        std::cout << (std::any_cast<bool>(value) ? "TRUE" : "FALSE") << std::endl;
+    }
+    else if (value.type() == typeid(Note)) {
+        Note currentNote = std::any_cast<Note>(value);
+        std::cout << "NOTE(" << currentNote.pitchClass << currentNote.octave << ")" << std::endl;
+    }
+    else if (value.type() == typeid(Instrument)) {
+        Instrument currentInstrument = std::any_cast<Instrument>(value);
+        std::cout << "INSTRUMENT(" << currentInstrument.name << ")" << std::endl;
+    }
+    else if (value.type() == typeid(std::vector<std::any>)) {
+        auto arrayElements = std::any_cast<std::vector<std::any>>(value);
+        std::cout << "[ ";
+
+        for (size_t i = 0; i < arrayElements.size(); i++) {
+            if (arrayElements[i].type() == typeid(int)) {
+                std::cout << std::any_cast<int>(arrayElements[i]);
+            }
+            else if (arrayElements[i].type() == typeid(double)) {
+                std::cout << std::any_cast<double>(arrayElements[i]);
+            }
+            else if (arrayElements[i].type() == typeid(std::string)) {
+                std::cout << "\"" << std::any_cast<std::string>(arrayElements[i]) << "\"";
+            }
+            else if (arrayElements[i].type() == typeid(char)) {
+                std::cout << "'" << std::any_cast<char>(arrayElements[i]) << "'";
+            }
+            else if (arrayElements[i].type() == typeid(Note)) {
+                Note n = std::any_cast<Note>(arrayElements[i]);
+                std::cout << n.pitchClass << n.octave;
+            }
+            else {
+                std::cout << "?";
+            }
+
+            if (i < arrayElements.size() - 1) std::cout << ", ";
+        }
+        std::cout << " ]" << std::endl;
+    }
+    else {
+        std::cout << "[Complex Object: SOUND]" << std::endl;
+    }
+
+    return {};
+}
+
+
+std::any TonInterpreter::visitSaveStat(TonParser::SaveStatContext *ctx) {
+
+    std::string rawFileName = ctx->STRING_VAL()->getText();
+    std::string fileName = rawFileName.substr(1, rawFileName.length() - 2);
+    std::any exportedValue = visit(ctx->expr());
+
+    if (exportedValue.type() == typeid(Sound)) {
+        Sound soundToSave = std::any_cast<Sound>(exportedValue);
+
+        AudioFile<double> audioFile;
+        
+        audioFile.setNumChannels(1);
+        audioFile.setNumSamplesPerChannel(soundToSave.samples.size());
+        audioFile.setSampleRate(soundToSave.sampleRate);
+
+        audioFile.samples[0] = soundToSave.samples;
+
+        bool success = audioFile.save(fileName);
+
+        if (success) {
+            std::cout << ">>> [SYSTEM] Successfully exported " << soundToSave.samples.size() 
+                      << " samples to: " << fileName << std::endl;
+        } else {
+            throw std::runtime_error("Error: Failed to write WAV file to disk.");
+        }
+    } 
+    else {
+        throw std::runtime_error("Error: !save command requires a SOUND type.");
+    }
+
+    return {};
 }
