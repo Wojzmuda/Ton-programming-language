3ba29237a0a18b629f179b009498ec105748861a
szymonw <handerdog3@gmail.com>
2026-06-16 21:25:34 +0200

    ANOTHER RANDOM DOT WTF

--- src/interpreter/TonInterpreter.cpp
+++ src/interpreter/TonInterpreter.cpp
@@ -1234,7 +1234,7 @@ std::any TonInterpreter::executeFunctionLogic(const std:: string& funcName, cons
             else if (expectedReturnType == "CHAR" && result.type() == typeid(char)) typeMatch = true;
             else if (expectedReturnType == "STRING" && result.type() == typeid(std::string)) typeMatch = true;
             else if (expectedReturnType == "ARRAY" && result.type() == typeid(std::vector<std::any>)) typeMatch = true;
-            else if (expectedReturnType == "NOTE" && result.type() == typeid(Note)) typeMatch = true;.
+            else if (expectedReturnType == "NOTE" && result.type() == typeid(Note)) typeMatch = true;
             else if (expectedReturnType == "SOUND" && result.type() == typeid(Sound)) typeMatch = true;
             else if (expectedReturnType == "INSTRUMENT" && result.type() == typeid(Instrument)) typeMatch = true;
             else if (expectedReturnType == "TIMELINE" && result.type() == typeid(Timeline)) typeMatch = true;
