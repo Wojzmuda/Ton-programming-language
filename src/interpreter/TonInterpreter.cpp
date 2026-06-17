d1b12471270792abf6857a455654c52c3e274505
Szymon Wołoszyn <handerdog3@gmail.com>
2026-06-17 17:58:04 +0200

    Issue67/implicit conversions (#112)
    * implicit conversions + example
    
    * explicit float <-> bool done
    
    * conversions docs done
    
    * conversion docs completed with example
    
    * remove todo on conversions

--- src/interpreter/TonInterpreter.cpp
+++ src/interpreter/TonInterpreter.cpp
@@ -27,21 +27,7 @@ const std::unordered_set<std::string> TonInterpreter::SYNTHS = {
     "square"
 };
 
-
-std::shared_ptr<Scope<std::any>> TonInterpreter::resolveScope(TonParser::TargetContext *ctx) {
-    int parentCount = ctx->elderRef().size();
-    auto targetScope = currentScope;
-    
-    for (int i = 0; i < parentCount; ++i) {
-        if (targetScope->parent == nullptr) {
-            size_t line = ctx->getStart()->getLine();
-            throw std::runtime_error("Error in line " + std::to_string(line) + ": 'parent::' reached beyond global scope.");
-        }
-        targetScope = targetScope->parent;
-    }
-    return targetScope;
-}
-bool TonInterpreter::doTypesMatch(const std::string &expectedTypeName, const std::any &value)
+bool TonInterpreter::coerceType(const std::string &expectedTypeName, std::any &value)
 {
     if (expectedTypeName == "INT" && value.type() == typeid(int)) return true;
     if (expectedTypeName == "NUMERICAL" && value.type() == typeid(double)) return true;
@@ -56,6 +42,47 @@ bool TonInterpreter::doTypesMatch(const std::string &expectedTypeName, const std
     if (expectedTypeName == "TRACK" && value.type() == typeid(Track)) return true;
     // edge case: empty strings or arrays that may come as empty `any` object.
     if (!value.has_value() && expectedTypeName != "VOID") return false;
+
+    // implicit conversions:
+    if (expectedTypeName == "INT") {
+        if (value.type() == typeid(double)) {
+            value = static_cast<int>(std::any_cast<double>(value));
+            return true;
+        }
+        if (value.type() == typeid(bool)) {
+            value = std::any_cast<bool>(value) ? 1 : 0;
+            return true;
+        }
+        if (value.type() == typeid(float)) {
+            value = static_cast<int>(std::any_cast<float>(value));
+            return true;
+        }
+    }
+    else if (expectedTypeName == "NUMERICAL") {
+        if (value.type() == typeid(int)) {
+            value = static_cast<double>(std::any_cast<int>(value));
+            return true;
+        }
+        if (value.type() == typeid(bool)) {
+            value = std::any_cast<bool>(value) ? 1.0 : 0.0;
+            return true;
+        }
+        if (value.type() == typeid(float)) {
+            value = static_cast<double>(std::any_cast<float>(value));
+            return true;
+        }
+    }
+    else if (expectedTypeName == "BOOL") {
+        if (value.type() == typeid(int)) {
+            value = (std::any_cast<int>(value) != 0); return true;
+        }
+        if (value.type() == typeid(double)) {
+            value = (std::any_cast<double>(value) != 0.0); return true;
+        }
+        if (value.type() == typeid(float)) {
+            value = (std::any_cast<float>(value) != 0.0f); return true;
+        }
+    }
     return false;
 }
 
@@ -167,7 +194,7 @@ std::any TonInterpreter::visitVarDecl(TonParser::VarDeclContext *ctx) {
 
     if (ctx->expr()) {
         value = visit(ctx->expr());
-        if (!doTypesMatch(typeName, value)) {
+        if (!coerceType(typeName, value)) {
             size_t line = ctx->getStart()->getLine();
             throw std::runtime_error("Line " + std::to_string(line) +
                 ": Cannot assign this value to a variable of type " +
@@ -275,10 +302,9 @@ std::any TonInterpreter::visitAssignment(TonParser::AssignmentContext *ctx) {
         if (!targetScope->exists(varName)) {
             throw std::runtime_error("Error: Variable '" + varName + "' must be declared first.");
         }
-std::any rightSide = visit(ctx->expr());
-        std::string declaredType = targetScope->resolveType(varName); 
-        
-        if (!doTypesMatch(declaredType, rightSide)) {
+        std::any rightSide = visit(ctx->expr());
+        std::string declaredType = currentScope->resolveType(varName);
+        if (!coerceType(declaredType, rightSide)) {
             size_t line = ctx->getStart()->getLine();
             throw std::runtime_error("Line " + std::to_string(line) +
                                      ": Cannot assign this value to a variable of type " +
@@ -510,11 +536,11 @@ std::any TonInterpreter::visitCreateSoundExpr(TonParser::CreateSoundExprContext 
     std::any arg1 = visit(ctx->expr(0));
     std::any arg2 = visit(ctx->expr(1));
 
-    if (!doTypesMatch("NOTE", arg1)) {
+    if (!coerceType("NOTE", arg1)) {
         size_t line = ctx->getStart()->getLine();
         throw std::runtime_error("Line " + std::to_string(line) + ": First argument of SOUND definition must be a NOTE.");
     }
-    if (!doTypesMatch("INT", arg2)) {
+    if (!coerceType("INT", arg2)) {
         size_t line = ctx->getStart()->getLine();
         throw std::runtime_error("Line " + std::to_string(line) + ": Second argument of SOUND definition must be an INT.");
     }
@@ -526,7 +552,7 @@ std::any TonInterpreter::visitCreateSoundExpr(TonParser::CreateSoundExprContext 
     if (ctx->expr().size() > 2) {
         std::any arg3 = visit(ctx->expr(2));
 
-        if (!doTypesMatch("NUMERICAL", arg3) && !doTypesMatch("INT", arg3)) {
+        if (!coerceType("NUMERICAL", arg3) && !coerceType("INT", arg3)) {
             size_t line = ctx->getStart()->getLine();
             throw std::runtime_error("Line " + std::to_string(line) + ": Volume must be a number in range [0, 2].");
         }
@@ -1285,7 +1311,7 @@ std::any TonInterpreter::executeFunctionLogic(const std::string& funcName, const
         std::string paramName = funcdefctx->ID(i+1)->getText();
         std::any argval = evaluatedArgs[i];
 
-        bool typeMatch = doTypesMatch(paramType, argval);
+        bool typeMatch = coerceType(paramType, argval);
         if (!typeMatch) {
             currentScope = previousScope;
             this->currentStackDepth--;
@@ -1315,7 +1341,7 @@ std::any TonInterpreter::executeFunctionLogic(const std::string& funcName, const
                 throw std::runtime_error("Function '" + funcName + "' must return a value of type " + expectedReturnType + ".");
             }
 
-            bool typeMatch = doTypesMatch(expectedReturnType, result);
+            bool typeMatch = coerceType(expectedReturnType, result);
             if (!typeMatch) {
                 throw std::runtime_error("Function '" + funcName + "' returned wrong type. Expected " + expectedReturnType + ".");
             }
@@ -1501,12 +1527,14 @@ std::any TonInterpreter::visitCastExpr(TonParser::CastExprContext *ctx) {
 
     else if (targetType == "NUMERICAL") {
         if (val.type() == typeid(int)) return static_cast<double>(std::any_cast<int>(val));
+        if (val.type() == typeid(bool)) return val;
         if (val.type() == typeid(double)) return val;
     }
 
 
     else if (targetType == "BOOL") {
         if (val.type() == typeid(int)) return std::any_cast<int>(val) != 0;
+        if (val.type() == typeid(double)) return std::any_cast<int>(val) != 0;
         if (val.type() == typeid(bool)) return val;
     }
 
@@ -1581,7 +1609,7 @@ std::any TonInterpreter::visitLengthOfExpr(TonParser::LengthOfExprContext *ctx) 
 
 
     size_t line = ctx->getStart()->getLine();
-    throw std::runtime_error("Runtime Error in line " + std::to_string(line) + 
+    throw std::runtime_error("Line " + std::to_string(line) +
                              ": LENGTH operator requires STRING, ARRAY, SOUND, TRACK, or TIMELINE.");
 }
 
--- src/typechecker/TonTypeChecker.cpp
+++ src/typechecker/TonTypeChecker.cpp
@@ -59,19 +59,21 @@ std::any TonTypeChecker::visitRelationalExpr(TonParser::RelationalExprContext *c
     if (hasUnknown(left, right)) {
         return std::string("BOOL");
     }
-    // TODO for now we are letting INT and NUMERICAL to be compared.
-    // Later on we should delete in because there will be implicit type conversion (int -> num)
+
+    if (isConvertible(left, right)) {
+        return std::string("BOOL");
+    }
+
     if (left == "SOUND" && right == "SOUND") {
         return std::string("BOOL");
     }
+
     if (left == "NOTE" && right == "NOTE") {
         return std::string("BOOL");
     }
-    if (left != right && !( (left == "INT" || left == "NUMERICAL") && (right == "INT" || right == "NUMERICAL") )) {
-        size_t line = ctx->getStart()->getLine();
-        throw std::runtime_error("Type Error in line " + std::to_string(line) + ": Cannot compare " + left + " with " + right);
-    }
-    return std::string("BOOL");
+
+    size_t line = ctx->getStart()->getLine();
+    throw std::runtime_error("Type Error in line " + std::to_string(line) + ": Cannot compare " + left + " with " + right);
 }
 
 std::any TonTypeChecker::visitUnaryExpr(TonParser::UnaryExprContext *ctx) {
@@ -212,20 +214,20 @@ std::any TonTypeChecker::visitCreateSoundExpr(TonParser::CreateSoundExprContext 
         return std::string("SOUND");
     }
 
-    if (arg1Type != "NOTE") {
+    if (!isConvertible("NOTE", arg1Type)) {
         size_t line = ctx->getStart()->getLine();
         throw std::runtime_error("Line " + std::to_string(line) +
                                  ": First argument of CreateSound must be a NOTE. Given: " + arg1Type);
     }
     
-    if (arg2Type != "INT") {
+    if (!isConvertible("INT", arg2Type)) {
         size_t line = ctx->getStart()->getLine();
         throw std::runtime_error("Line " + std::to_string(line) +
                                  ": Second argument (duration) of CreateSound must be an INT. Given: " + arg2Type);
     }
 
     if (ctx->expr().size() > 2) {
-        if (arg3Type != "INT" && arg3Type != "NUMERICAL") {
+        if (!isConvertible("NUMERICAL", arg3Type)) {
             size_t line = ctx->getStart()->getLine();
             throw std::runtime_error("Line " + std::to_string(line) +
                                      ": Third argument (volume) of SOUND must be INT or NUMERICAL. Given: " + arg3Type);
@@ -382,11 +384,11 @@ std::any TonTypeChecker::visitCastExpr(TonParser::CastExprContext *ctx) {
     }
 
     else if (targetType == "NUMERICAL") {
-        if (exprType == "INT" || exprType == "NUMERICAL") return std::string("NUMERICAL");
+        if (exprType == "INT" || exprType == "NUMERICAL" || exprType == "BOOL") return std::string("NUMERICAL");
     }
    
     else if (targetType == "BOOL") {
-        if (exprType == "INT" || exprType == "BOOL") return std::string("BOOL");
+        if (exprType == "INT" || exprType == "BOOL" || exprType == "NUMERICAL") return std::string("BOOL");
     }
 
     else if (targetType == "STRING") {
